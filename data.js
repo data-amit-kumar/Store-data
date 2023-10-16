2093d96ce63d9b525808b4b38718fa1f3cc8a71c
data-amit-kumar <data2.amitkumar@gmail.com>
2023-10-16 22:04:50 +0530

    change4

--- data.js
+++ data.js
@@ -20,6 +20,36 @@ function store(e){
         storedUsers.push(Person);
         localStorage.setItem('users', JSON.stringify(storedUsers));
       }
+
+      showUsers();
 }
 
+function showUsers() {
+    var users = JSON.parse(localStorage.getItem('users'));
+    var usersList = document.getElementById('usersList');
+    usersList.innerHTML = '';
 
+    if (users !== null) {
+      users.forEach(function (Person, index) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(Person.fname + '  ' + Person.lname));
+
+        var deleteBtn = document.createElement('button');
+        deleteBtn.appendChild(document.createTextNode('X'));
+        deleteBtn.onclick = function () {
+          deleteUser(index);
+        };
+        li.appendChild(deleteBtn);
+
+        usersList.appendChild(li);
+      });
+    }
+  }
+
+  function deleteUser(index) {
+    var users = JSON.parse(localStorage.getItem('users'));
+    users.splice(index, 1);
+    localStorage.setItem('users', JSON.stringify(users));
+    showUsers();
+  }
+  showUsers();
